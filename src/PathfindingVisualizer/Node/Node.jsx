5923047085914d15927d5f4c638fd0165783c083
syrillo <72819915+syrillo@users.noreply.github.com>
2021-12-18 13:03:08 -0500

    redo

---
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+import './Node.css';
+
+export default class Node extends Component {
+  render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      row,
+    } = this.props;
+    const extraClassName = isFinish
+      ? 'node-finish'
+      : isStart
+      ? 'node-start'
+      : '';
+
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}></div>
+    );
+  }
+}
